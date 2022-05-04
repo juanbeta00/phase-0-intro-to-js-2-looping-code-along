6e9e179aa49261b1eafc1b3de09ddcf27081edca
JUAN <juanbeta00@hotmail.com>
2022-05-03 22:07:15 -0400

    commit tree da23e8dd731f7c5f542cf854f67128155fad2d08

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ['juan', 'vicky', 'mario'];
+
+function writeCards(names, event) {
+    let messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+const number = [''];
+function countDown(number) {
+    
+    while (number > 0) {
+        
+        console.log(`${number}`, --number);
+    }
+
+}
+
